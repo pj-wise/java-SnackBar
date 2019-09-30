d65d69d134f9ff32c5c39d30aba5e626bda62969
pj-wise <pjwisedev@gmail.com>
2019-09-30 17:40:54 -0400

    finished vending machine and customer classes

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,31 @@
 package snackBarApp;
 
 public class Customer {
-    
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private double cash;
+
+    public Customer(String name, double cash) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.cash = cash;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addCash(double cash) {
+        this.cash = this.cash + cash;
+    }
+
+    public void buy(double Cash) {
+        this.cash = this.cash - cash;
+    }
 }
--- src/VendingMaching.java
+++ src/VendingMaching.java
@@ -8,9 +8,24 @@ public class VendingMachine {
 
     private String name;
 
+    //constructor
     public VendingMachine(String name) {
         maxId++;
         id = maxId;
         this.name = name;
     }
+
+    //getters & setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
